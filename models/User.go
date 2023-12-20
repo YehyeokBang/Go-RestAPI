c1d5917f7163cc59837257466d281f108342f278
Yehyeok Bang <qkddpgur318@gmail.com>
2023-12-20 21:45:44 +0900

    style: remove unnecessary comments

--- models/User.go
+++ models/User.go
@@ -1,9 +1,29 @@
 package models
 
+import "golang.org/x/crypto/bcrypt"
+
 type User struct {
 	ID       uint   `json:"id" gorm:"primary_key"`
 	Name     string `json:"name"`
 	Nickname string `json:"nickname"`
 	Email    string `json:"email" gorm:"unique"`
 	Password string `json:"password"`
+}
+
+func GetUserByEmail(email string) (User, error) {
+	var user User
+	result := DB.Where("email = ?", email).First(&user)
+	return user, result.Error
+}
+
+func HashPassword(password string) (string, error) {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", err
+	}
+	return string(hashedPassword), nil
+}
+
+func VerifyPassword(hashedPassword, inputPassword string) error {
+	return bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(inputPassword))
 }
