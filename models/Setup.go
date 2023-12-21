49146a2200e049393e24206342d8b4d742f2d584
Yehyeok Bang <qkddpgur318@gmail.com>
2023-12-21 18:01:45 +0900

    feat: migrate database from SQLite to MySQL

--- models/Setup.go
+++ models/Setup.go
@@ -1,15 +1,34 @@
 package models
 
 import (
-	"gorm.io/driver/sqlite"
+	"fmt"
+	"os"
+
+	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+
+	"github.com/joho/godotenv"
 )
 
 var DB *gorm.DB
 
 func ConnectDatabase() {
+	loadErr := godotenv.Load(".env")
 
-	database, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if loadErr != nil {
+		panic(loadErr)
+	}
+
+	MYSQL_HOST := os.Getenv("MYSQL_HOST")
+	MYSQL_PORT := os.Getenv("MYSQL_PORT")
+	MYSQL_USER := os.Getenv("MYSQL_USER")
+	MYSQL_PASSWORD := os.Getenv("MYSQL_PASSWORD")
+	MYSQL_DATABASE := os.Getenv("MYSQL_DATABASE")
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		MYSQL_USER, MYSQL_PASSWORD, MYSQL_HOST, MYSQL_PORT, MYSQL_DATABASE)
+
+	database, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
 		panic("Failed to connect to database!")
